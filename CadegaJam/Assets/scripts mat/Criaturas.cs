7b690488aba730ba43d09456dbbaa04faf858ccf
Theus02 <matheus.felipe1751@gmail.com>
2021-09-25 11:41:16 -0300

    teste

---
+++ CadegaJam/Assets/scripts mat/Criaturas.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public enum faction
+{
+    Allies, Enemy, Turret
+}
+
+public class Criaturas : MonoBehaviour
+{
+
+    public faction type;
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
---
+++ CadegaJam/Assets/scripts mat/detecta.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class detecta : MonoBehaviour
+{
+    public List<faction> AlvosPossiveis = new List<faction>();
+    public List<Criaturas> Targets = new List<Criaturas>();
+    public Criaturas AlvoAtual;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.GetComponent<Criaturas>()) {
+            Criaturas criaturaNoAlcance = collision.gameObject.GetComponent<Criaturas>();
+
+            if (Targets.Contains(criaturaNoAlcance) == false && AlvosPossiveis.Contains(criaturaNoAlcance.type))  
+            {
+                Targets.Add(criaturaNoAlcance);
+            }
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.GetComponent<Criaturas>())
+        {
+            Criaturas criaturaNoAlcance = collision.gameObject.GetComponent<Criaturas>();
+
+            if (Targets.Contains(criaturaNoAlcance))
+            {
+                Targets.Remove(criaturaNoAlcance);
+            }
+        }
+    }
+
+    private void Escolhe()
+    {
+        if (Targets.Contains(AlvoAtual) == false )
+        {
+            AlvoAtual = null;
+        }
+
+        if (Targets.Count > 0 && AlvoAtual == null) 
+        {
+            int RandomTarget = Random.Range(0,Targets.Count + 1);
+            Debug.Log(RandomTarget);
+            AlvoAtual = Targets[RandomTarget];
+        }
+    }
+    private void Update()
+    {
+        Escolhe();
+    }
+}
